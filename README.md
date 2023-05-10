02f9eb579f5ce0768b484a025d2b36746fb28dad
coolidev <coolidev@hotmail.com>
2023-05-10 11:37:34 -0400

    flutter login ui

---
+++ README.md
@@ -0,0 +1,5 @@
+# Login UI
+
+A login UI screen in Flutter, a mobile sdk framework by Google.
+
+[Login UI Flutter](https://raw.githubusercontent.com/coolidev/flutter-login-ui/master/art/thumbnail.png)
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\envirionment\Android\Sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\MY_WORKS\my-git-ropo\flutter\flutter-login-ui-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\envirionment\Android\Sdk\flutter\bin\cache\artifacts\engine\ios"
